8445157f2377ffb12448cdd52acc9e764877f5a3
bayuiqballl <bayuiqballl13@gmail.com>
2020-04-27 14:01:46 +0700

    soal 3

--- soal3/main.go
+++ soal3/main.go
@@ -5,12 +5,12 @@ import "fmt"
 func main() {
 
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 {
-			fmt.Println("Fizz")
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
 		} else if i%5 == 0 {
 			fmt.Println("Buzz")
-		} else if i%15 == 0 {
-			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
 		} else {
 			fmt.Println(i)
 		}
