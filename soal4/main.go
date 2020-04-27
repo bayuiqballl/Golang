ff1c5baf58e6056a86245fa886256a24821a5659
bayuiqballl <bayuiqballl13@gmail.com>
2020-04-27 14:04:24 +0700

    soal4

---
+++ soal4/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	sekarang := time.Now()
+
+	fmt.Println("Hari ini : ", sekarang.Format("2020, April 27"))
+
+	panjang := time.Date(2020, time.March, 1, 0, 0, 0, 0, time.UTC)
+
+	perbedaan := sekarang.Sub(panjang)
+
+	hari := int(perbedaan.Hours() / 24)
+
+	fmt.Printf("%d\n", hari)
+}
